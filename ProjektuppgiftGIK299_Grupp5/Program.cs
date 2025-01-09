2097b0c4dda48733b427996ed95408cd401c349e
Elvira Aspelin <h24elasp@du.se>
2025-01-09 10:23:43 +0100

    Välkommstmedelande

--- ProjektuppgiftGIK299_Grupp5/Program.cs
+++ ProjektuppgiftGIK299_Grupp5/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine();
+        Console.WriteLine(new string('*', 40));
+        Console.WriteLine();
+        Console.WriteLine("Välkommen till Däckbytaren 2000!");
+        Console.WriteLine();
+        Console.WriteLine(new string('*', 40));
     }
 }

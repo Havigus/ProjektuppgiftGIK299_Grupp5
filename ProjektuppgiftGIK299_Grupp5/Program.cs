02d3de1d9225f5373f79a82711928559cdd8f207
viktor <viktoreriksson@live.se>
2025-01-09 14:48:43 +0100

    Implementerat case 1

--- ProjektuppgiftGIK299_Grupp5/Program.cs
+++ ProjektuppgiftGIK299_Grupp5/Program.cs
@@ -29,30 +29,76 @@ class Program
             Console.WriteLine("Gör ditt val med en siffra: ");
             Console.WriteLine();
 
-            switch (Console.ReadKey().KeyChar.ToString())
+            switch (Console.ReadKey(intercept: true).KeyChar.ToString())
             {
                 case "1":
-                    Console.WriteLine("Kundnamn: ");
-                    string name = Console.ReadLine();
-                    Console.WriteLine("Bilregistreringsnummer: ");
-                    string carRegNr = Console.ReadLine();
-                    Console.WriteLine("Datum och tid: (skriv som yyyy,mm,dd hh:mm");
-                    while (!DateTime.TryParse(Console.ReadLine(), out DateTime dateTime))
+                    while (true)
                     {
-                        Console.WriteLine("Ogiltigt datum och tid format.");
+                        Console.WriteLine("Kundnamn: ");
+                        string name = Console.ReadLine();
+                        Console.WriteLine("Bilregistreringsnummer: ");
+                        string carRegNr = Console.ReadLine();
+                        Console.WriteLine("Datum och tid: (skriv som yyyy,mm,dd hh:mm");
+                        DateTime dateTime;
+                        while (!DateTime.TryParse(Console.ReadLine(), out dateTime))
+                        {
+                            Console.WriteLine("Ogiltigt datum och tid format.");
+                        }
+
+                        //TODO! Lägg denhär menyn i en while loop så man kan välja fler services. Behövs nog en List för att hålla valen
+                        //alt att man bara tar servicen som en string och skippar enum
+                        Console.WriteLine("""
+                                          Vilken tjänst?
+                                          1. DäckbyteSäsong
+                                          2. DäckbyteNyaDäck
+                                          3. Hjulinställning
+                                          4. Däckhotell
+                                          5. EfterdragningDäck
+                                          6. BeställaDäck
+                                          """);
+                        Services service;
+                        while (true)
+                        {
+                            var key = Console.ReadKey(intercept: true).KeyChar;
+
+                            if (int.TryParse(key.ToString(), out int num) && Enum.IsDefined(typeof(Services), num))
+                            {
+                                service = (Services)num;
+                                break;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Ogiltigt val.");
+                            }
+                        }
+
+                        string comment = null;
+                        Console.WriteLine("Vill du lägga till en kommentar? Y/N");
+                        if (Console.ReadKey(intercept: true).KeyChar.ToString().ToUpper() == "Y")
+                        {
+                            comment = Console.ReadLine();
+                        }
+
+                        Console.WriteLine($"""
+                                          Bekräfta uppgifterna.
+                                          Kundnamn: {name}
+                                          Bilregistreringsnummer: {carRegNr}
+                                          Datum och tid: {dateTime}
+                                          Service: {service}
+                                          Kommentar: {comment}
+                                          
+                                          Stämmer dethär? Y/N
+                                          """);
+                        if (Console.ReadKey(intercept: true).KeyChar.ToString().ToUpper() == "Y")
+                        {
+                            adminPanel.AddBooking(name, carRegNr, dateTime, service, comment);
+                            break;
+                        }
+
                     }
+
                     
-                   //TODO! Lägg denhär menyn i en while loop så man kan välja fler services. Behövs nog en List för att hålla valen
-                    //alt att man bara tar servicen som en string och skippar enum
-                    Console.WriteLine("""
-                                      Vilken tjänst?
-                                      1. DäckbyteSäsong
-                                      2. DäckbyteNyaDäck
-                                      3. Hjulinställning
-                                      4. Däckhotell
-                                      5. EfterdragningDäck
-                                      6. BeställaDäck
-                                      """);
+                    
                     
                     
                     
